729c3342839155bf6376b79e86261ae0414018d4
Michael Limb <something15525@gmail.com>
2013-12-17 14:28:47 -0800

    Telephony: Dismissable voicemail notifications
    Ported from cm-10.2:
    http://review.cyanogenmod.org/#/c/35023
    
    Change-Id: Iccd01210cdd9a43167392fc09debbfc4cb23b6f6

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -848,7 +848,6 @@ public class NotificationMgr {
             if (vibrate) {
                 notification.defaults |= Notification.DEFAULT_VIBRATE;
             }
-            notification.flags |= Notification.FLAG_NO_CLEAR;
             configureLedNotification(mContext, VOICEMAIL_NOTIFICATION, notification);
             mNotificationManager.notify(VOICEMAIL_NOTIFICATION, notification);
         } else {
